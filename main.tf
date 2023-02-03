47802403b10f4f23be4386c7f02d91d245df98b3
ankupsatpute <95556178+ankupsatpute@users.noreply.github.com>
2023-02-03 11:30:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "myFirstInstance" {
     Name = var.tag_name
   }
 provisioner "local-exec" {
-    command = "echo ${self.private_ip} >> /etc/ansible/hosts"
+    command = "echo ${self.private_ip} >> deploy.ini"
   }
 }
 
