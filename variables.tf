80721d87807f5e3e2869e3f212eda4c067ca5e85
ankupsatpute <95556178+ankupsatpute@users.noreply.github.com>
2023-02-03 15:58:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,8 +5,8 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  #default     =  "LTIDEMO"
-  public_key     =  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDM19Ir3cWSbg1OMbXfOcBszjNzGAf7uMtWcYcaI+kUN4J+2NRr84fVHjBrJh1tqkWdG2rt6+S9fvuFbPEobxDhbrkiqyZGxF3nroYFzAskotatbzM+mmbARq48T0WdGTdcVECVR6bFIgN1YFwi61XRCsdS4lYIQLdv1yMrL3wFjclsDJCl8DXF2H2TpAqhQXYQqK5G6jNOYbTZAzO7bH/1ykMPGYufAACAijh6YS6n5NJRALprrAaPEaFkiVD0GDCMjF/cQDqXxGnnnt/vWyWSFZGnHLBNSKVTdZYcJp4+KCTw31deRloMT1Fgc59Yq6nZFHKPb8VeCBNofn2gddoQ480PtqQvvu+53T0sUVPSj993pSXHANA/p1qq5mIFFp98FkEZtnDZp2P1bqdtuNGOYvfqNXDBDQz4Kqu0/yvjeoGTfSnFpH2jpMsLj/ob07j5DhOFZnIM1dHL9oegALg7HJ5TFH8NuFL3FIllc/GlIBq0ryit0++n3VRbccw5CzM= ubuntu@ip-172-31-32-151"
+  default     =  "New1"
+ // public_key     =  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDM19Ir3cWSbg1OMbXfOcBszjNzGAf7uMtWcYcaI+kUN4J+2NRr84fVHjBrJh1tqkWdG2rt6+S9fvuFbPEobxDhbrkiqyZGxF3nroYFzAskotatbzM+mmbARq48T0WdGTdcVECVR6bFIgN1YFwi61XRCsdS4lYIQLdv1yMrL3wFjclsDJCl8DXF2H2TpAqhQXYQqK5G6jNOYbTZAzO7bH/1ykMPGYufAACAijh6YS6n5NJRALprrAaPEaFkiVD0GDCMjF/cQDqXxGnnnt/vWyWSFZGnHLBNSKVTdZYcJp4+KCTw31deRloMT1Fgc59Yq6nZFHKPb8VeCBNofn2gddoQ480PtqQvvu+53T0sUVPSj993pSXHANA/p1qq5mIFFp98FkEZtnDZp2P1bqdtuNGOYvfqNXDBDQz4Kqu0/yvjeoGTfSnFpH2jpMsLj/ob07j5DhOFZnIM1dHL9oegALg7HJ5TFH8NuFL3FIllc/GlIBq0ryit0++n3VRbccw5CzM= ubuntu@ip-172-31-32-151"
   
 }
 
